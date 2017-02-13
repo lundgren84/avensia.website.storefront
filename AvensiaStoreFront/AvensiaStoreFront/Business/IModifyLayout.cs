bb74f2e2e05e04a1c3d46475b52d440c1fd6dd01
Hampus Lundgren <lundgren84@hotmail.se>
2017-02-13 10:15:35 +0100

    Added project

---
+++ AvensiaStoreFront/AvensiaStoreFront/Business/IModifyLayout.cs
@@ -0,0 +1,13 @@
+using AvensiaStoreFront.Models.ViewModels;
+
+namespace AvensiaStoreFront.Business
+{
+    /// <summary>
+    /// Defines a method which may be invoked by PageContextActionFilter allowing controllers
+    /// to modify common layout properties of the view model.
+    /// </summary>
+    interface IModifyLayout
+    {
+        void ModifyLayout(LayoutModel layoutModel);
+    }
+}
---
+++ AvensiaStoreFront/AvensiaStoreFront/Business/Rendering/IContainerPage.cs
@@ -0,0 +1,9 @@
+namespace AvensiaStoreFront.Business.Rendering
+{
+    /// <summary>
+    /// Marker interface for content types which should not be handled by DefaultPageController.
+    /// </summary>
+    interface IContainerPage
+    {
+    }
+}
